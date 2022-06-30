89b30f4c681e7899636a93092731864f5f3c565d
Mohammad Faizan <mohammad.faizansaif@gmail.com>
2022-06-30 17:01:27 +0530

    Filter interviews on min and avg rating

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -5,13 +5,36 @@ class Interview < ApplicationRecord
   accepts_nested_attributes_for :interview_rounds, allow_destroy: true, reject_if: :all_blank
 
   def status
-    "sac" #interview_rounds.last.status
+    status = interview_rounds.select {|r| r.status}.last
+    return "Unknown" if status.nil?
+    InterviewRound::Status.fetch(status, "Unknown")
   end
 
   def average_ratings
     all_ratings = interview_rounds.map{|i| i.rating}
     all_ratings.compact!
-    return 0 if (all_ratings == nil or all_ratings.empty?)
+    return 0 if (all_ratings.nil? or all_ratings.empty?)
     (all_ratings.inject(:+)/all_ratings.length.to_f).round(1)
+  end
+
+  def self.filter(min_rating, avg_rating)
+    # find interviews having atleast 3 rounds
+    interviews = Interview.joins(:interview_rounds).group("interviews.id").having("count(interview_rounds.id) >= ?", 3).pluck(:id)
+
+    # Find associated interview rounds
+    interview_rounds = InterviewRound.where(interview_id: interviews).pluck(:id)
+
+    # Find skill ratings
+    rounds_rating_below_2 = SkillRating.where(interview_round_id: interview_rounds)
+                                      .where('rating < ?', min_rating)
+                                      .pluck(:interview_round_id)
+
+    eligible_rounds = (interview_rounds - rounds_rating_below_2.uniq)
+
+    final_result = Interview.includes(interview_rounds: :skill_ratings).joins(interview_rounds: :skill_ratings)
+                            .where("interview_rounds.id IN (#{eligible_rounds.join(',')})")
+
+    # filter and return interviews having average >= 3
+    final_result.select {|i| i.average_ratings >= 3}
   end
 end
--- app/models/interview_round.rb
+++ app/models/interview_round.rb
@@ -2,6 +2,8 @@ class InterviewRound < ApplicationRecord
   belongs_to :interview
   belongs_to :employee
   has_many :skill_ratings
+  scope :scheduled, -> { where(status: STATUS[1]) }
+  scope :done, -> { where(status: STATUS[2]) }
 
   STATUS = {
     1 => "Scheduled",
@@ -10,7 +12,7 @@ class InterviewRound < ApplicationRecord
 
   def rating
     ratings = skill_ratings.pluck(:rating)
-    return nil if (ratings == nil or ratings.empty?)
+    return nil if (ratings.nil? or ratings.empty?)
     (ratings.inject(:+)/ratings.length.to_f).round(1)
   end
 end
